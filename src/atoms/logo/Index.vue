b8481a431475047c59128e618d1a1930109d3d49
Guilherme-Lopes-dev <guilherme.cpmg@hotmail.com>
2024-05-07 10:27:22 -0300

    first commit

---
+++ src/atoms/logo/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <img :src="LogoImg" class="header_logo" alt="Logo Cilia" />
+</template>
+
+<script>
+import LogoImg from '../../assets/logo.svg'
+import './logo.scss'
+export default {
+  name: 'LogoImg',
+
+  setup(){
+    return{
+      LogoImg,
+    }
+  }
+
+}
+</script>
+
+<style >
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <NavBar/>
+</template>
+
+<script>
+import NavBar from './NavBar.vue'
+export default {
+  name: 'HelloWorld',
+  components:{
+  NavBar,
+  },
+
+  data() {
+
+  }
+}
+</script>
+
+<style >
+</style>
